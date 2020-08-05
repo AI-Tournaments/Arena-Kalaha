aba0e717a03761370f48927c56bd8a1c82920aee
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-05 09:41:45 +0200

    Added check for time violation.

--- arena.js
+++ arena.js
@@ -89,11 +89,12 @@ function sumScore(scoreArray, gameboardLength, startValue, aiList){
 }
 function callAI(matchList, matchIndex, aiIndex, data){
 	let match = matchList[matchIndex];
-	let participant = match.ai[aiIndex%2];
+	let participant = match.participants[aiIndex%2];
 	let worker = participant.worker;
 	if(worker instanceof Worker){
 		if(worker.onmessage === null){
 			worker.onmessage = messageEvent => {
+				worker_real.lastCalled = undefined;
 				worker.onmessage = undefined;
 				let selectedMove = messageEvent.data;
 				let moveData = doMove(match.gameboard, selectedMove, match.settings.rules);
@@ -114,6 +115,9 @@ function callAI(matchList, matchIndex, aiIndex, data){
 							match.gameboard.push(match.gameboard.shift());
 						}
 					}
+					match.participants.forEach(participant => {
+						participant.worker.lastCalled = null;
+					});
 					match.score = sumBoard(match.gameboard);
 					let done = true;
 					let scoreArray = [];
@@ -133,17 +137,31 @@ function callAI(matchList, matchIndex, aiIndex, data){
 				postMessage({type: 'DNF', message: {name: participant.name, error: errorEvent.message}});
 			}
 		}
+		worker_real.lastCalled = new Date().getTime();
 		worker.postMessage({
 			gameboard: match.gameboard,
 			settings: match.settings,
-			opponent: match.settings.general.displayOpponents ? match.ai[(aiIndex+1)%2].name : null
+			opponent: match.settings.general.displayOpponents ? match.participants[(aiIndex+1)%2].name : null
 		});
 	}else{
 		worker.then(worker_real => {
-			match.ai[aiIndex%2].worker = worker_real;
+			match.participants[aiIndex%2].worker = worker_real;
 			callAI(matchList, matchIndex, aiIndex, data);
 		});
 	}
+}
+function executionWatcher(executionLimit=1000, participants=[]){
+	participants.forEach(participant => {
+		let executionTimeViolation = participant.worker.lastCalled === undefined ? false : executionLimit < new Date().getTime() - participant.worker.lastCalled;
+		if(participant.worker.lastCalled === null || executionTimeViolation){
+			participants.splice(participants.indexOf(participant), 1);
+			if(executionTimeViolation){
+				postMessage({type: 'DNF', message: {name: participant.name, error: 'Execution time violation.'}});
+				participant.worker.terminate();
+			}
+		}
+	});
+	setTimeout(executionWatcher, executionLimit, participants, executionLimit);
 }
 onmessage = messageEvent => {
 	let gameboard = [];
@@ -154,12 +172,13 @@ onmessage = messageEvent => {
 		gameboard.push(0);
 	}
 	let matchList = [];
+	let participants=[];
 	let participant_1 = messageEvent.data.arena.participants[0][0];
 	let participant_2 = messageEvent.data.arena.participants[1][0];
 	for(let i = 0; i < 2; i++){
 		while(matchList.length < messageEvent.data.arena.settings.general.averageOf){
-			matchList.push({
-				ai: [
+			let match = {
+				participants: [
 					{
 						worker: createWorkerFromRemoteURL(participant_1.url, true),
 						name: participant_1.name
@@ -172,12 +191,15 @@ onmessage = messageEvent => {
 				history: [],
 				gameboard: gameboard.slice(),
 				settings: messageEvent.data.arena.settings
-			});
+			};
+			participants.concat(match.participants);
+			matchList.push(match);
 			callAI(matchList, matchList.length-1, 0, messageEvent.data);
 		}
 		let temp = participant_1;
 		participant_1 = participant_2;
 		participant_2 = temp;
 	}
+	executionWatcher(messageEvent.data.arena.settings.general.timelimit_ms, participants);
 	postMessage({type: 'Pending', message: matchList.length});
 }
