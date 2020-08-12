67eac5272acbcae17dd3c088d1a8c8b84c29b402
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-12 16:17:21 +0200

    Swap order.

--- arena.js
+++ arena.js
@@ -1,6 +1,6 @@
 'use strict'
-importScripts('https://chrisacrobat.github.io/js-compilation/CreateWorkerFromRemoteURL.js');
 importScripts('https://ai-tournaments.github.io/AI-Tournaments/Arena/participants.js');
+importScripts('https://chrisacrobat.github.io/js-compilation/CreateWorkerFromRemoteURL.js');
 function isGameFinished(gameboard){
 	let ai_1 = 0;
 	let ai_2 = 0;
