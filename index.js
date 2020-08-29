3ad7260d3bf9f40fb47de4bc132d3e8217726121
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-29 22:57:55 +0200

    Added replay.

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 'use strict'
 function a(){
 	let data = JSON.parse(decodeURI(location.hash.substring(1)));
-	console.log(data);
-	let baseUp = data.settings.gameboard.boardLength;
-	let baseDown = baseUp*2;
-
+	let firstMover = data.log[0].mover;
+	console.log(data); // TODO: REMOVE!
+	let baseDown = data.settings.gameboard.boardLength;
+	let baseUp = baseDown*2 + 1;
+	let slider = document.getElementById('slider');
+	{
+		let first = data.log[0].mover;
+		document.getElementById('first-player').innerHTML = first;
+		let secund = document.getElementById('secund-player')
+		for(let index = 0; index < data.log.length; index++) {
+			const log = data.log[index];
+			if(log.mover !== first){
+				secund.innerHTML = log.mover;
+			}
+		}
+		if(secund.innerHTML === ''){
+			secund.innerHTML = first;
+		}
+	}
+	let buttonBack = document.getElementById('step-back');
+	let buttonNext = document.getElementById('step-next');
+	buttonBack.addEventListener('click', step);
+	buttonNext.addEventListener('click', step);
+	slider.max = data.log.length;
+	slider.addEventListener('input', event=>{
+		setBoard(slider.valueAsNumber - 1);
+	});
 	window.onresize = resizeGameboard;
-	init();
+	setBoard();
+	function step(mouseEvent){
+		slider.valueAsNumber += mouseEvent.target === buttonNext ? 1 : -1;
+		setBoard(slider.valueAsNumber - 1);
+		buttonBack.disabled = false;
+		buttonNext.disabled = false;
+		if(mouseEvent.target === buttonBack){
+			buttonBack.disabled = slider.valueAsNumber === 0;
+		}
+		if(mouseEvent.target === buttonNext){
+			buttonNext.disabled = slider.valueAsNumber === data.log.length;
+		}
+	}
+	function setBoard(logIndex=-1){
+		let log = -1 < logIndex ? data.log[logIndex] : null;
+		let state = log !== null ? log.gameboard.slice() : null;
+		if(log !== null && log.mover !== firstMover){
+			for(let i = 0; i < state.length/2; i++) {
+				state.push(state.shift())
+			}
+		}
+		// Rezero base columns.
+		document.getElementById('column-up').innerHTML = '<div id="square_' + baseUp + '" class="square square-up">' + (state === null ? 0 : state[baseUp]) + '</div>';
+		document.getElementById('column-down').innerHTML = '<div id="square_' + baseDown + '" class="square">' + (state === null ? 0 : state[baseDown]) + '</div>';
 
+		// Recreate up and down rows.
+		let rowUp = document.getElementById('rowUp');
+		let rowDown = document.getElementById('rowDown');
+		rowUp.innerHTML = '';
+		rowDown.innerHTML = '';
+		let boardSize = data.settings.gameboard.boardLength;
+		let startValue = data.settings.gameboard.startValue;
+		for(let index = 0; index < boardSize; index++){
+			let indexUp = (boardSize*2 - index);
+			let indexDown = index;
+			rowUp.innerHTML += '<div id="square_' + indexUp + '" class="square square-up">' + (state === null ? startValue : state[indexUp]) + '</div>';
+			rowDown.innerHTML += '<div id="square_' + indexDown + '" class="square">' + (state === null ? startValue : state[indexDown]) + '</div>';
+		}
+
+		// Customize squares
+		//let baseUpSquare = document.getElementById('square_' + baseUp);
+		//let baseDownSquare = document.getElementById('square_' + baseDown);
+		//baseUpSquare.style.backgroundColor = colorBaseUp; // TODO: Set team color.
+		//baseDownSquare.style.backgroundColor = colorBaseDown; // TODO: Set team color.
+
+		resizeGameboard();
+	}
 	function resizeGameboard(){
 		let gameboard = document.getElementById('gamebord');
 		let allSquares = [...document.getElementsByClassName('square')];
@@ -24,29 +92,5 @@ function a(){
 		}
 		let zoom = gameboard.parentElement.offsetWidth / gameboard.offsetWidth;
 		gameboard.style.zoom = zoom;
-	}
-
-	function init(){
-		// Rezero base columns.
-		document.getElementById('column-up').innerHTML = '<div id="square_' + baseUp + '" class="square square-up">0</div>';
-		document.getElementById('column-down').innerHTML = '<div id="square_' + baseDown + '" class="square">0</div>';
-
-		// Recreate up and down rows.
-		let rowUp = document.getElementById('rowUp');
-		let rowDown = document.getElementById('rowDown');
-		rowUp.innerHTML = '';
-		rowDown.innerHTML = '';
-		for(let index = 0; index < data.settings.boardLength; index++){
-			rowUp.innerHTML += '<div id="square_' + (boardSize*2 + 1 - index) + '" class="square square-up">' + startValue + '</div>';
-			rowDown.innerHTML += '<div id="square_' + (index + 1) + '" class="square">' + startValue + '</div>';
-		}
-
-		// Customize squares
-		//let baseUpSquare = document.getElementById('square_' + baseUp);
-		//let baseDownSquare = document.getElementById('square_' + baseDown);
-		//baseUpSquare.style.backgroundColor = colorBaseUp; // TODO: Set team color.
-		//baseDownSquare.style.backgroundColor = colorBaseDown; // TODO: Set team color.
-
-		resizeGameboard();
 	}
 }
